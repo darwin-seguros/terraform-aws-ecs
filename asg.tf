fa93560e3c9d10f00cf9b9530a0997dea0d5aab0
Rafael Ladislau <rafa.ladis@gmail.com>
2023-05-22 15:10:18 -0300

    CI: Adding spot allocation strategy (lower-price)

--- asg.tf
+++ asg.tf
@@ -21,6 +21,7 @@ resource "aws_autoscaling_group" "ecs" {
       spot_instance_pools                      = 3
       on_demand_base_capacity                  = var.on_demand_base_capacity
       on_demand_percentage_above_base_capacity = var.on_demand_percentage
+      spot_allocation_strategy                 = "lowest-price"
     }
   }
 
